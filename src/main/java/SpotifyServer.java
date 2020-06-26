87a94ced5b9256a71915f2f73748012c63df3a9c
kAndris2 <k.andris258@gmail.com>
2020-06-26 14:39:36 +0200

    spoty

---
+++ src/main/java/SpotifyServer.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class SpotifyServer {
+    ArrayList<Song> songs = new ArrayList<Song>();
+
+    public SpotifyServer() {
+
+    }
+
+    public ArrayList<Song> getTrackList() { return songs; }
+}
